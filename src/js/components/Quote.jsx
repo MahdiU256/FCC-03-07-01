e7d69898ff7cbdac641694cce954a021269fef77
Mahdi Uddin <mahdiuddin256@gmail.com>
2019-12-19 11:11:12 +0000

    Quote Component - v00

---
+++ src/js/components/Quote.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Quote extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <React.Fragment>
+                <header className="header">App Header</header>
+                <section className="body">
+                    <h1 className="title">App Title</h1>
+                </section>
+                <footer className="footer">App Footer</footer>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Quote;
