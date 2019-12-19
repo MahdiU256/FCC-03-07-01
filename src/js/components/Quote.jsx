379f59801633cc57df3417bd32e47043ca2f61d6
Mahdi Uddin <mahdiuddin256@gmail.com>
2019-12-19 14:43:44 +0000

    Quote Component v01 - Added state object

--- src/js/components/Quote.jsx
+++ src/js/components/Quote.jsx
@@ -1,21 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Quote extends Component {
-    constructor(props) {
-        super(props);
-    }
-    
-    render() {
-        return (
-            <React.Fragment>
-                <header className="header">App Header</header>
-                <section className="body">
-                    <h1 className="title">App Title</h1>
-                </section>
-                <footer className="footer">App Footer</footer>
-            </React.Fragment>
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			quote: '',
+			author: ''
+		};
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<header className="header">App Header</header>
+				<section className="body">
+					<h1 className="title">App Title</h1>
+				</section>
+				<footer className="footer">App Footer</footer>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Quote;
