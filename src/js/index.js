237762b5716fa85ba26c2fe2360874d746a15009
Mahdi Uddin <mahdiuddin256@gmail.com>
2019-12-19 15:08:55 +0000

    User Story #1 complete

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,5 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import Quote from './components/Quote.jsx';
 
-const wrapper = document.getElementById('app');
+const wrapper = document.getElementById('quote-box');
 wrapper ? ReactDOM.render(<Quote />, wrapper) : false && console.log(false);
