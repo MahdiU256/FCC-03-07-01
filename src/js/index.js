578d05405897b6b465c1c658815ed99952c78fda
Mahdi Uddin <mahdiuddin256@gmail.com>
2019-12-19 11:31:26 +0000

    Entry JS File v00 - Debug 01

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Quote from './components/Quote';
+import Quote from './components/Quote.jsx';
 
 const wrapper = document.getElementById('app');
 wrapper ? ReactDOM.render(<Quote />, wrapper) : false && console.log(false);
