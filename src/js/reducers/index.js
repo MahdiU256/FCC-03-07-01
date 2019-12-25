c83e88de9818f9cef0e6fd1dda49d16bfa12b989
Mahdi Uddin <mahdiuddin256@gmail.com>
2019-12-25 17:23:51 +0000

    Reducer v01

---
+++ src/js/reducers/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+    quote: '',
+    author: ''
+};
+
+function rootReducer(state = initialState, action) {
+    switch (action.type) {
+        
+        default:
+            return state;
+            break;
+    }
+}
+
+export default rootReducer;
