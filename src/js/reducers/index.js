9e7859a07aa8917bd253d5c1dbd1d8f16d91c895
Mahdi Uddin <mahdiuddin256@gmail.com>
2019-12-25 18:07:51 +0000

    Reducer v02

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,3 +1,5 @@
+import { DATA_FETCHED, API_ERROR } from "../constants/action-types";
+
 const initialState = {
     quote: '',
     author: ''
@@ -5,7 +7,17 @@ const initialState = {
 
 function rootReducer(state = initialState, action) {
     switch (action.type) {
-        
+        case DATA_FETCHED: 
+            return {
+                quote: action.payload[0].quote,
+                author: action.payload[0].character,
+            }
+            break;
+        case API_ERROR: 
+            return {
+                error: action.payload
+            }
+            break;
         default:
             return state;
             break;
