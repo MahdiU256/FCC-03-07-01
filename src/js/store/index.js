d78d74f59cdbc9053fea2e0efd459aa5eea939db
Mahdi Uddin <mahdiuddin256@gmail.com>
2019-12-25 18:04:29 +0000

    Store v02

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,8 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers/index.js';
+import { dataRequest } from '../actions/index.js';
+import apiSaga from '../sagas/api-saga.js';
 
-const store = createStore(rootReducer);
+const initialiseSagaMiddleware = createSagaMiddleware();
 
-store = window.store;
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, storeEnhancers(applyMiddleware(initialiseSagaMiddleware)));
+
+initialiseSagaMiddleware.run(apiSaga);
+
+window.store = store;
+window.dataRequest = dataRequest;
 
 export default store;
