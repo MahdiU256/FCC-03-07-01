f795937dacf7b7637df434a70d60a0e6b57405fe
Mahdi Uddin <mahdiuddin256@gmail.com>
2020-01-15 13:58:18 +0000

    QuoteBox v02

--- src/js/containers/QuoteBox.jsx
+++ src/js/containers/QuoteBox.jsx
@@ -1,31 +1,28 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import Quote from '../components/Quote.jsx';
-import { dataRequest } from "../actions/index.js";
+import { dataRequest } from '../actions/index.js';
 
-class QuoteBox extends Component {
-	componentDidMount() {
-		this.props.actions.dataRequest();
-	}
-	
+export class QuoteBox extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<Quote />
+				<Quote actions={dataRequest}/>
 			</React.Fragment>
-		)
+		);
 	}
 }
 
-const mapStateToProps = (state, ownProps) => {
-	return { 
+const mapStateToProps = (state) => {
+	return {
 		quote: state.quote,
-		author: state.author 
+		author: state.author
 	};
-}
+};
 
 const mapDispatchToProps = (dispatch) => {
-	return { actions: bindActionCreators(dataRequest, dispatch) }
-}
+	return { actions: bindActionCreators(dataRequest, dispatch) };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(QuoteBox)
+export default connect(mapStateToProps, mapDispatchToProps)(Quote);
