275cc4dcb0fbe43b6d224b1b1a042fa390d693d3
Mahdi Uddin <mahdiuddin256@gmail.com>
2020-01-15 14:00:53 +0000

    API Saga v02

--- src/js/sagas/api-saga.js
+++ src/js/sagas/api-saga.js
@@ -1,3 +1,4 @@
+import 'react';
 import { takeEvery, call, put } from 'redux-saga/effects';
 import { DATA_REQUESTED, DATA_FETCHED, API_ERROR } from '../constants/action-types.js';
 
@@ -10,12 +11,11 @@ function* workerSaga() {
 		const payload = yield call(getData);
 		yield put({ type: DATA_FETCHED, payload });
 	} catch (err) {
+		console.error(err);
 		yield put({ type: API_ERROR, payload: err });
 	}
 }
 
-async function getData() {
-	const res = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
-    const data = await res.json();
-    return data;
+function getData() {
+	return fetch('https://thesimpsonsquoteapi.glitch.me/quotes').then((res) => res.json());
 }
