d797d39a6bf5fcab1c76dd6132fb7880c00e7a25
Mahdi Uddin <mahdiuddin256@gmail.com>
2020-01-15 14:00:26 +0000

    Entry JS File v02

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import Quote from './components/Quote.jsx';
+import store from './store/index.js';
+import QuoteBox from './containers/QuoteBox.jsx';
 
 const wrapper = document.getElementById('quote-box');
-wrapper ? ReactDOM.render(<Quote />, wrapper) : false && console.log(false);
+wrapper ? ReactDOM.render(
+<Provider store={store}>
+    <QuoteBox />
+</Provider>
+, wrapper) : false && console.log(false);
