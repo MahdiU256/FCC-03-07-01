6d15498da94325660771e2b140dc563f93fe2c93
Mahdi Uddin <mahdiuddin256@gmail.com>
2020-01-15 13:59:33 +0000

    Quote Component v02

--- src/js/components/Quote.jsx
+++ src/js/components/Quote.jsx
@@ -1,32 +1,8 @@
 import React, { Component } from 'react';
 
 class Quote extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			quote: '',
-			author: ''
-		};
-
-		this.onClick = this.onClick.bind(this);
-	}
-
-	async componentDidMount() {
-		const res = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
-		const data = await res.json();
-		this.setState({
-			quote: data[0].quote,
-			author: data[0].character
-		});
-	}
-
-	async onClick() {
-		const res = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
-		const data = await res.json();
-		this.setState({
-			quote: data[0].quote,
-			author: data[0].character
-		});
+	componentDidMount() {
+		setTimeout(this.props.actions, 250);
 	}
 
 	render() {
@@ -35,22 +11,26 @@ class Quote extends Component {
 				<header className="header">Random Quote Machine</header>
 				<article className="body">
 					<section className="quotes">
-						<p id="text">{this.state.quote}</p>
-						<span id="author">{this.state.author}</span>
+						<p id="text">{this.props.quote}</p>
+						<span id="author">{this.props.author}</span>
 					</section>
 					<section className="click">
 						<a id="tweet-quote" href="twitter.com/intent/tweet">
 							Tweet Quote
 						</a>
-						<button id="new-quote" onClick={this.onClick}>
+						<button id="new-quote" onClick={this.props.actions}>
 							New Quote
 						</button>
 					</section>
 				</article>
 				<footer className="footer">
 					Created by Mahdi Uddin, 2019. The API used in this project was{' '}
-					<a className="ref" href="https://thesimpsonsquoteapi.glitch.me/">The Simpsons Quote API</a>, created by{' '}
-					<a className="ref" href="https://jluboff-portfolio.glitch.me/">Jason Luboff</a>.
+					<a className="ref" href="https://thesimpsonsquoteapi.glitch.me/">
+						The Simpsons Quote API
+					</a>, created by{' '}
+					<a className="ref" href="https://jluboff-portfolio.glitch.me/">
+						Jason Luboff
+					</a>.
 				</footer>
 			</React.Fragment>
 		);
